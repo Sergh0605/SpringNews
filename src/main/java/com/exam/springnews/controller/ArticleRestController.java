c4c6f7df516dc3309513997fd0f01a6417a71a01
Sergh0605 <puzakovsergei@gmail.com>
2021-11-08 22:14:37 +0300

    Rest Controllers Complete

--- src/main/java/com/exam/springnews/controller/ArticleRestController.java
+++ src/main/java/com/exam/springnews/controller/ArticleRestController.java
@@ -1,20 +1,23 @@
 package com.exam.springnews.controller;
 
-import com.exam.springnews.dto.FileUploadDto;
 import com.exam.springnews.service.ArticlesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/articles")
 public class ArticleRestController {
 
     private final ArticlesService articlesService;
+    private final HttpServletRequest request;
 
-    public ArticleRestController(ArticlesService articlesService) {
+    public ArticleRestController(ArticlesService articlesService, HttpServletRequest request) {
         this.articlesService = articlesService;
+        this.request = request;
     }
 
     @GetMapping("/")
@@ -33,13 +36,17 @@ public class ArticleRestController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<?> uploadArticle(@RequestParam("file") MultipartFile file, @RequestParam("authorId") Long authorId, @RequestParam("category") String category) {
+    public ResponseEntity<?> uploadArticle(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("authorId") Long authorId,
+            @RequestParam("category") String category) {
+        String realPathToUploads = request.getServletContext().getRealPath("");
         return ResponseEntity.
                 status(HttpStatus.CREATED).
                 body(articlesService.createArticle(
                         file,
                         category,
                         authorId,
-                        "dsdsd"));
+                        realPathToUploads));
     }
 }
